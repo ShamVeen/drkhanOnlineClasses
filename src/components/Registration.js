98d54386c81280fe8bc017c95a8b5c0a94eb0df2
ShamVeen <118723873+ShamVeen@users.noreply.github.com>
2023-04-07 20:44:48 +0530

    Update Registration.js

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -9,7 +9,7 @@ function Registration() {
         <img src={freeclass} />
       </div>
 
-      <div className='register-form'>
+      <div className="register-form">
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSfrXWpioMvcbBgGrkoOZSxWlAw3q7nzzUoe_3Pe-b-UIQVLqQ/viewform?embedded=true"
           width="1340"
@@ -20,6 +20,18 @@ function Registration() {
         >
           Loading…
         </iframe>
+      </div>
+      <div
+      className='register-number'
+        style={{
+          marginTop: '40px',
+          textAlign: 'center',
+        }}
+      >
+        <p style={{ fontSize: '20px', fontWeight: '600' }}>
+          <u>Or Just Talk to us:</u> +971 502614700 DUBAI, +966 599661580 UAE,
+          +91 6265526339 INDIA
+        </p>
       </div>
     </div>
   );
